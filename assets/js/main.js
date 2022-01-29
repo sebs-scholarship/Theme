a2eb4fee6e772ecb34e17bdcf1e2c5fcd119ce7b
Levi Muniz <levi.muniz17@gmail.com>
2022-01-28 23:39:19 -0700

    Even more semicolons

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
     setTimeout(function () {
       const loader = $('#pb_loader');
       if (loader.length > 0) {
-        loader.removeClass('show')
+        loader.removeClass('show');
       }
-    }, 500)
+    }, 500);
   }
-  loader()
+  loader();
 
   // scroll
   const scrollWindow = function () {
@@ -22,35 +22,35 @@ $(document).ready(function () {
 
       if (st > 150) {
         if (!navbar.hasClass('scrolled')) {
-          navbar.addClass('scrolled')
+          navbar.addClass('scrolled');
         }
       }
       if (st < 150) {
         if (navbar.hasClass('scrolled')) {
-          navbar.removeClass('scrolled sleep')
+          navbar.removeClass('scrolled sleep');
         }
       }
       if (st > 350) {
         if (!navbar.hasClass('awake')) {
-          navbar.addClass('awake')
+          navbar.addClass('awake');
         }
 
         if (sd.length > 0) {
-          sd.addClass('sleep')
+          sd.addClass('sleep');
         }
       }
       if (st < 350) {
         if (navbar.hasClass('awake')) {
-          navbar.removeClass('awake')
-          navbar.addClass('sleep')
+          navbar.removeClass('awake');
+          navbar.addClass('sleep');
         }
         if (sd.length > 0) {
-          sd.removeClass('sleep')
+          sd.removeClass('sleep');
         }
       }
-    })
+    });
   }
-  scrollWindow()
+  scrollWindow();
 
   // slick sliders
   function slickSliders () {
@@ -62,64 +62,64 @@ $(document).ready(function () {
       autoplaySpeed: 7000,
       arrows: false,
       draggable: false
-    })
+    });
   }
-  slickSliders()
+  slickSliders();
 
   // navigation
   const OnePageNav = function () {
     $(".smoothscroll[href^='#'], #probootstrap-navbar ul li a[href^='#']").on('click', function (e) {
-      e.preventDefault()
+      e.preventDefault();
       const hash = this.hash;
       const navToggler = $('.navbar-toggler');
       $('html, body').animate({
 
         scrollTop: $(hash).offset().top
       }, 700, 'easeInOutExpo', function () {
-        window.location.hash = hash
-      })
+        window.location.hash = hash;
+      });
 
       if (navToggler.is(':visible') && !$(this).hasClass('no-nav-expand')) {
-        navToggler.click()
+        navToggler.click();
       }
-    })
+    });
   }
-  OnePageNav()
+  OnePageNav();
 
   const offCanvasNav = function () {
     const toggleNav = $('.js-pb_nav-toggle');
     const offcanvasNav = $('.js-pb_offcanvas-nav_v1');
     if (toggleNav.length > 0) {
       toggleNav.click(function (e) {
-        $(this).toggleClass('active')
-        offcanvasNav.addClass('active')
-        e.preventDefault()
-      })
+        $(this).toggleClass('active');
+        offcanvasNav.addClass('active');
+        e.preventDefault();
+      });
     }
     offcanvasNav.click(function (e) {
       if (offcanvasNav.hasClass('active')) {
-        offcanvasNav.removeClass('active')
-        toggleNav.removeClass('active')
+        offcanvasNav.removeClass('active');
+        toggleNav.removeClass('active');
       }
-      e.preventDefault()
-    })
+      e.preventDefault();
+    });
   }
-  offCanvasNav()
+  offCanvasNav();
 
   function updateYear () {
     const year = $('#year');
-    year.text(new Date().getFullYear())
+    year.text(new Date().getFullYear());
   }
 
-  updateYear()
-})
+  updateYear();
+});
 
 $(window).on('load', function () {
   const loader = function () {
     const loader = $('#pb_loader');
     if (loader.length > 0) {
-      loader.removeClass('show')
+      loader.removeClass('show');
     }
   }
-  loader()
-})
+  loader();
+});
